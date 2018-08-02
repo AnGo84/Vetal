4305cef729964e7f63dfc01e0769d6f0a9bceae0
AnGo <ango1984@gmail.com.ua>
2018-08-02 17:01:30 +0300

    added filter part for mainPage added open folder logic

--- src/main/java/ua/com/vetal/controller/MainController.java
+++ src/main/java/ua/com/vetal/controller/MainController.java
@@ -1,11 +1,13 @@
 package ua.com.vetal.controller;
 
+import java.io.IOException;
 import java.security.Principal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 //import org.springframework.security.core.userdetails.User;
@@ -15,8 +17,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import ua.com.vetal.service.TaskServiceImpl;
+import ua.com.vetal.utils.FileUtils;
+import ua.com.vetal.utils.PlatformUtils;
 import ua.com.vetal.utils.WebUtils;
 
 @Controller
@@ -75,6 +80,22 @@ public class MainController {
 		model.addAttribute("userInfo", userInfo);
 
 		return "userInfoPage";
+	}
+
+	@RequestMapping(value = "/openFolder", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void openFolder(Model model, Principal principal) {
+		// logger.info("Try open folder");
+		String url = "/home/mbrunarskiy/Desktop";
+		logger.info("Try open folder " + url + " for OS: " + PlatformUtils.osName());
+		try {
+
+			FileUtils.openDirectory(url);
+		} catch (IOException e) {
+			logger.info("Error on open: " + url);
+			e.printStackTrace();
+		}
+		// return "redirect/main";
 	}
 
 	@RequestMapping(value = "/403", method = RequestMethod.GET)
---
+++ src/main/java/ua/com/vetal/utils/FileUtils.java
@@ -0,0 +1,171 @@
+package ua.com.vetal.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * Created by AnGo on 30.05.2017.
+ */
+public class FileUtils {
+	/**
+	 * Gets the base location of the given class.
+	 * <p>
+	 * If the class is directly on the file system (e.g.,
+	 * "/path/to/my/package/MyClass.class") then it will return the base
+	 * directory (e.g., "file:/path/to").
+	 * </p>
+	 * <p>
+	 * If the class is within a JAR file (e.g.,
+	 * "/path/to/my-jar.jar!/my/package/MyClass.class") then it will return the
+	 * path to the JAR (e.g., "file:/path/to/my-jar.jar").
+	 * </p>
+	 *
+	 * @param c
+	 *            The class whose location is desired.
+	 * @see FileUtils#urlToFile(URL) to convert the result to a {@link File}.
+	 */
+	public static URL getLocation(final Class<?> c) {
+		if (c == null)
+			return null; // could not load the class
+
+		// try the easy way first
+		try {
+			final URL codeSourceLocation = c.getProtectionDomain().getCodeSource().getLocation();
+			if (codeSourceLocation != null)
+				return codeSourceLocation;
+		} catch (final SecurityException e) {
+			// NB: Cannot access protection domain.
+		} catch (final NullPointerException e) {
+			// NB: Protection domain or code source is null.
+		}
+
+		// NB: The easy way failed, so we try the hard way. We ask for the class
+		// itself as a resource, then strip the class's path from the URL
+		// string,
+		// leaving the base path.
+
+		// get the class's raw resource path
+		final URL classResource = c.getResource(c.getSimpleName() + ".class");
+		if (classResource == null)
+			return null; // cannot find class resource
+
+		final String url = classResource.toString();
+		final String suffix = c.getCanonicalName().replace('.', '/') + ".class";
+		if (!url.endsWith(suffix))
+			return null; // weird URL
+
+		// strip the class's path from the URL string
+		final String base = url.substring(0, url.length() - suffix.length());
+
+		String path = base;
+
+		// remove the "jar:" prefix and "!/" suffix, if present
+		if (path.startsWith("jar:"))
+			path = path.substring(4, path.length() - 2);
+
+		try {
+			return new URL(path);
+		} catch (final MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Converts the given {@link URL} to its corresponding {@link File}.
+	 * <p>
+	 * This method is similar to calling {@code new File(url.toURI())} except
+	 * that it also handles "jar:file:" URLs, returning the path to the JAR
+	 * file.
+	 * </p>
+	 *
+	 * @param url
+	 *            The URL to convert.
+	 * @return A file path suitable for use with e.g. {@link FileInputStream}
+	 * @throws IllegalArgumentException
+	 *             if the URL does not correspond to a file.
+	 */
+	public static File urlToFile(final URL url) {
+		return url == null ? null : urlToFile(url.toString());
+	}
+
+	/**
+	 * Converts the given URL string to its corresponding {@link File}.
+	 *
+	 * @param url
+	 *            The URL to convert.
+	 * @return A file path suitable for use with e.g. {@link FileInputStream}
+	 * @throws IllegalArgumentException
+	 *             if the URL does not correspond to a file.
+	 */
+	public static File urlToFile(final String url) {
+		String path = url;
+		if (path.startsWith("jar:")) {
+			// remove "jar:" prefix and "!/" suffix
+			final int index = path.indexOf("!/");
+			path = path.substring(4, index);
+		}
+		try {
+			if (PlatformUtils.isWindows() && path.matches("file:[A-Za-z]:.*")) {
+				path = "file:/" + path.substring(5);
+			}
+			return new File(new URL(path).toURI());
+		} catch (final MalformedURLException e) {
+			// NB: URL is not completely well-formed.
+		} catch (final URISyntaxException e) {
+			// NB: URL is not completely well-formed.
+		}
+		if (path.startsWith("file:")) {
+			// pass through the URL as-is, minus "file:" prefix
+			path = path.substring(5);
+			return new File(path);
+		}
+		throw new IllegalArgumentException("Invalid URL: " + url);
+	}
+
+	/**
+	 * Gets the directory from given File {@link File}.
+	 *
+	 * @param file
+	 *            The File to extract.
+	 * @return A file path suitable for use with e.g. {@link FileInputStream}
+	 */
+	public static File getDirectory(final File file) {
+		if (file == null || !file.exists())
+			return null;
+		return file.isDirectory() ? file : file.getParentFile();
+	}
+
+	public static File getFileWithName(final Class<?> c, String fileName) {
+		URL url = FileUtils.getLocation(c);
+		File f = new File(FileUtils.getDirectory(FileUtils.urlToFile(url)) + File.separator + fileName);
+		return f;
+	}
+
+	public static void openDirectory(String path) throws IOException {
+		File file = new File(path);
+		if (file.exists()) {
+			if (!file.isDirectory()) {
+				file = getDirectory(file);
+			}
+			// Runtime.getRuntime().exec("explorer.exe /select," +
+			// file.getPath());
+			if (PlatformUtils.isWindows()) {
+				Runtime.getRuntime().exec("explorer.exe " + file.getPath());
+			}
+			if (PlatformUtils.isLinux()) {
+				Runtime.getRuntime().exec("gnome-open " + file.getPath());
+			}
+		}
+	}
+
+	public static String fileNameWithDir(String directoryName, String fileName) {
+
+		return directoryName + File.separator + fileName;
+	}
+
+}
