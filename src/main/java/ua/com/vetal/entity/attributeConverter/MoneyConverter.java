fa625c24eca6fdf7a3acc3b8e7bff07ed809cdf6
AnGo84 <ango1984@gmail.com>
2020-05-05 21:38:01 +0300

    Fix MoneyConverter

--- src/main/java/ua/com/vetal/entity/attributeConverter/MoneyConverter.java
+++ src/main/java/ua/com/vetal/entity/attributeConverter/MoneyConverter.java
@@ -1,24 +1,32 @@
 package ua.com.vetal.entity.attributeConverter;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 @Converter
+@Slf4j
 public class MoneyConverter implements AttributeConverter<Double, Long> {
 	@Override
 	public Long convertToDatabaseColumn(Double aDouble) {
+		log.info("convertToDatabaseColumn Double: {}", aDouble);
 		if (aDouble == null) {
 			return new Long(0);
 		}
-		return Double.valueOf(aDouble * 100).longValue();
+		Long result = Double.valueOf(aDouble * 100).longValue();
+		log.info("Get Long: {}", result);
+		return result;
 	}
 
 	@Override
 	public Double convertToEntityAttribute(Long aLong) {
+		log.info("convertToDatabaseColumn Long: {}", aLong);
 		if (aLong == null) {
 			return Double.valueOf(0);
 		}
-		aLong = aLong / 100;
-		return aLong.doubleValue();
+		Double result = (aLong.doubleValue()) / 100;
+		log.info("Get Double: {}", result);
+		return result;
 	}
 }
